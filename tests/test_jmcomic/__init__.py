23fdf19df4846c7de4dc725b25d997f4d4c981cf
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-04-03 20:27:49 +0800

    增加测试代码、测试工作流、Release和发布PYPI工作流。

---
+++ tests/test_jmcomic/__init__.py
@@ -0,0 +1,51 @@
+import unittest
+
+# noinspection PyUnresolvedReferences
+import jmcomic
+from jmcomic import *
+
+
+class JmTestConfigurable(unittest.TestCase):
+    option: JmOption = None
+    client: JmcomicClient = None
+
+    def setUp(self) -> None:
+        print_sep('>')
+
+    def tearDown(self) -> None:
+        print_sep('<')
+
+    @classmethod
+    def setUpClass(cls):
+        # 获取项目根目录
+        application_workspace = os.path.abspath(os.path.dirname(__file__) + '/../..')
+
+        # 设置 workspace → assets/
+        set_application_workspace(f'{application_workspace}/assets/')
+        # 设置 实体类的save_dir → assets/download
+        WorkEntity.detail_save_base_dir = workspace("/download/", is_dir=True)
+
+        # 设置 JmOption，JmcomicClient
+        option = cls.use_option('option_test.yml')
+        cls.option = option
+        cls.client = option.build_jm_client()
+
+        # 更新
+        cls.try_update_jm_cookies()
+
+    @staticmethod
+    def use_option(op_filename: str) -> JmOption:
+        return create_option(workspace(f"/config/{op_filename}"))
+
+    @staticmethod
+    def move_workspace(new_dir: str):
+        set_application_workspace(workspace(f"/{new_dir}/", is_dir=True))
+
+    @classmethod
+    def try_update_jm_cookies(cls):
+        # 尝试更新 cookies
+        cookies = ChromePluginCookieParser({'remember', 'comic'}) \
+            .apply(when_valid_message="更新jmcomic-option成功！！！！")
+        if cookies is not None:
+            cls.option.client_config['meta_data']['cookies'] = cookies
+            cls.option.save_to_file()
---
+++ tests/test_jmcomic/test_jm_client.py
@@ -0,0 +1,61 @@
+from test_jmcomic import *
+
+
+class Test_Client(JmTestConfigurable):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.move_workspace('download')
+
+    def test_download_from_cdn_directly(self):
+        photo_id = 'JM15193'
+        option = self.option
+        cdn_crawler = option.build_cdn_crawler()
+        cdn_crawler.download_photo_from_cdn_directly(
+            option.build_cdn_request(photo_id),
+        )
+
+    def test_download_image(self):
+        jm_photo_id = 'JM15193'
+        photo_detail = self.client.get_photo_detail(jm_photo_id)
+        self.client.download_by_image_detail(
+            photo_detail[0],
+            img_save_path=workspace('test_download_image.png')
+        )
+
+    def test_get_album_detail_by_jm_photo_id(self):
+        album_id = "JM15193"
+        print_obj_dict(self.client.get_album_detail(album_id))
+
+    def test_get_photo_detail_by_jm_photo_id(self):
+        """
+        测试通过 JmcomicClient 和 jm_photo_id 获取 JmPhotoDetail对象
+        """
+        jm_photo_id = 'JM15193'
+        photo_detail = self.client.get_photo_detail(jm_photo_id)
+        photo_detail.when_del_save_file = True
+        photo_detail.after_save_print_info = True
+        del photo_detail
+
+    def test_multi_album_and_single_album(self):
+        multi_photo_album_id = [
+            "195822",
+        ]
+
+        for album_id in multi_photo_album_id:
+            album_detail: JmAlbumDetail = self.client.get_album_detail(album_id)
+            print(f'本子: [{album_detail.title}] 一共有{album_detail.page_count}页图')
+
+    def test_search(self):
+        jm_search_page = self.client.search_album('MANA')
+        for album_id in jm_search_page.album_id_iter():
+            print(album_id)
+
+    def test_gt_300_photo(self):
+        photo_id = '147643'
+        photo_detail: JmPhotoDetail = self.client.get_photo_detail(photo_id)
+        for image_detail in photo_detail:
+            image_detail: JmImageDetail
+            print(image_detail.img_url)
+        self.client.download_by_image_detail(photo_detail[3000], workspace('3000.png'))
