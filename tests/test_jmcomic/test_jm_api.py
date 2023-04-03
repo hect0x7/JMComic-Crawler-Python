51c9dff0d76e9205a699284f4d5adcd20b6a6847
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-04-03 21:44:54 +0800

    fix tests code

--- tests/test_jmcomic/test_jm_api.py
+++ tests/test_jmcomic/test_jm_api.py
@@ -19,8 +19,6 @@ class Test_Api(JmTestConfigurable):
         """
         测试jmcomic模块的api的使用
         """
-        set_global_proxy()
-        # self.client.add_meta_data('verify', False)
         album_id_ls = [
             # '219757',
             '15193',
@@ -28,11 +26,6 @@ class Test_Api(JmTestConfigurable):
         ]
 
         jmcomic.download_album(album_id_ls[0], self.option)
-        # multi_thread_launcher(
-        #     iter_objs=album_id_ls,
-        #     apply_each_obj_func=lambda album_id: jmcomic.download_album(album_id, self.option),
-        #     wait_finish=True
-        # )
 
     def test_batch(self):
         album_ls = str_to_list('''
