cce669b211ea9b09521dd3447e100750e232749a
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-05-25 22:41:00 +0800

    fix

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -48,7 +48,8 @@ def hook_debug(option):
 
     class HookDebugClient(JmHtmlClient):
 
-        def raise_request_error(self, resp):
+        @classmethod
+        def raise_request_error(cls, resp):
             from common import write_text, fix_windir_name, format_ts
             write_text(
                 f'{jm_download_dir}/[请求禁漫失败时的网页内容]_[{format_ts}]_[{fix_windir_name(resp.url)}].html',
