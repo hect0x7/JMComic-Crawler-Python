cb6130e668c45d5e83dc3b880f9fab1cebc8e291
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-06-23 10:03:29 +0800

    fix test code

--- tests/test_jmcomic/test_jm_api.py
+++ tests/test_jmcomic/test_jm_api.py
@@ -103,5 +103,5 @@ class Test_Api(JmTestConfigurable):
 
     def test_get_jmcomic_url(self):
         print(JmModuleConfig.get_jmcomic_domain_all())
-        print(JmModuleConfig.get_jmcomic_url(self.client))
-        print(JmModuleConfig.get_jmcomic_domain_all(self.client))
+        print(self.client.get_jmcomic_url())
+        print(self.client.get_jmcomic_domain_all())
