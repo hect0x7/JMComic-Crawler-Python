5117e4440bedfdc9f6180fb0e0780da368e0183e
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-07-14 22:03:24 +0800

    fix:(

--- src/jmcomic/jm_client_impl.py
+++ src/jmcomic/jm_client_impl.py
@@ -246,7 +246,7 @@ class JmHtmlClient(AbstractJmClient):
             """
             使用此方法包装 self.get
             """
-            resp = JmImageResp(self.get(url, params={'v': format_ts()}))
+            resp = JmImageResp(self.get(url, params={'v': time_stamp()}))
 
             if resp.is_success:
                 return resp
