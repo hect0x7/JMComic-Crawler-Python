5c5fb067512ab68a22f04fe34ee2367efb4142df
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-08-03 16:58:32 +0800

    v2.1.7

--- src/jmcomic/__init__.py
+++ src/jmcomic/__init__.py
@@ -2,7 +2,7 @@
 # 被依赖方 <--- 使用方
 # config <--- entity <--- toolkit <--- client <--- option <--- downloader
 
-__version__ = '2.1.6'
+__version__ = '2.1.7'
 
 # noinspection PyUnresolvedReferences
 from common import *
