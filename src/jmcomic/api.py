fc5194015f318a9cf0f298e662ae7d7e9223ad09
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-08-16 20:38:49 +0800

    v2.1.12: 支持实体类的切片语法，JmDownloader增加下载前Filter过滤机制 (#96)

--- src/jmcomic/api.py
+++ src/jmcomic/api.py
@@ -1,24 +1,29 @@
 from .jm_downloader import *
 
 
-def download_album_batch(jm_album_id_iter: Union[Iterable, Generator],
-                         option=None,
-                         ):
+def download_batch(download_api,
+                   jm_id_iter: Union[Iterable, Generator],
+                   option=None,
+                   ):
     """
-    批量下载album.
-    一个album，对应一个线程，对应一个option
+    批量下载 album / photo
+    一个album/photo，对应一个线程，对应一个option
 
-    @param jm_album_id_iter: album_id的迭代器
-    @param option: 下载选项，为空默认是 JmOption.default()
+    @param download_api: 下载api
+    @param jm_id_iter: jmid (album_id, photo_id) 的迭代器
+    @param option: 下载选项，对所有的jmid使用同一个，默认是 JmOption.default()
     """
     from common import multi_thread_launcher
 
+    if option is None:
+        option = JmOption.default()
+
     return multi_thread_launcher(
         iter_objs=set(
-            JmcomicText.parse_to_album_id(album_id)
-            for album_id in jm_album_id_iter
+            JmcomicText.parse_to_album_id(jmid)
+            for jmid in jm_id_iter
         ),
-        apply_each_obj_func=lambda aid: download_album(aid, option),
+        apply_each_obj_func=lambda aid: download_api(aid, option),
     )
 
 
@@ -31,7 +36,7 @@ def download_album(jm_album_id, option=None):
     """
 
     if not isinstance(jm_album_id, (str, int)):
-        return download_album_batch(jm_album_id, option)
+        return download_batch(download_album, jm_album_id, option)
 
     with new_downloader(option) as dler:
         dler.download_album(jm_album_id)
@@ -41,6 +46,9 @@ def download_photo(jm_photo_id, option=None):
     """
     下载一个章节
     """
+    if not isinstance(jm_photo_id, (str, int)):
+        return download_batch(download_photo, jm_photo_id, option)
+
     with new_downloader(option) as dler:
         dler.download_photo(jm_photo_id)
 
