de074f82e09477a037a692a8447e4a6936dac48a
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-08-29 20:34:27 +0800

    【GitHub Actions】把域名配置为禁漫的特定可用域名 (#111)
    GitHub Actions重新可用

--- tests/test_jmcomic/test_jm_api.py
+++ tests/test_jmcomic/test_jm_api.py
@@ -104,9 +104,9 @@ class Test_Api(JmTestConfigurable):
     def test_get_jmcomic_url(self):
         func_list = {
             self.client.get_jmcomic_url,
-            self.client.get_jmcomic_domain_all,
+            # self.client.get_jmcomic_domain_all,
             JmModuleConfig.get_jmcomic_url,
-            JmModuleConfig.get_jmcomic_domain_all,
+            # JmModuleConfig.get_jmcomic_domain_all,
         }
 
         exception_list = []
