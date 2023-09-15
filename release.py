e0fe081fec06ca54a857fdb8aca4dfaba56f5ad0
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-09-15 16:15:16 +0800

    test

--- release.py
+++ release.py
@@ -3,10 +3,12 @@ import sys
 import re
 
 def add_output(k, v):
+    print(f'set {k} = {v}')
     print(os.system(f'echo {k}={v} >> $GITHUB_OUTPUT'))
 
 
 msg = sys.argv[1]
+print(f'msg: {msg}')
 p = re.compile('(.*?): ?(.*)')
 match = p.search(msg)
 assert match is not None, f'commit message format is wrong: {msg}'
