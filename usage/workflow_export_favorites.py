0d0f3e9fc99afc5191d459845477066dba588ba7
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-12-18 01:33:51 +0800

    impl env

--- usage/workflow_export_favorites.py
+++ usage/workflow_export_favorites.py
@@ -2,12 +2,23 @@ from jmcomic import *
 
 
 def prepare_actions_input_and_secrets():
-    # JmcomicText.match_os_env
-    pass
+    def env(match: Match) -> str:
+        name = match[1]
+        value = os.getenv(name, None)
+        if value is not None:
+            return value
+
+        # 未配置secrets，尝试从工作流中取
+        value = os.getenv(f'IN_{name}', '')
+        # 工作流也没有传值
+        ExceptionTool.require_true(value != '', f'未配置secrets或工作流，字段为: {name}')
+        return value
+
+    JmcomicText.dsl_replacer.add_dsl_and_replacer(r'\$\{(.*?)\}', env)
 
 
 def main():
-    JmcomicText.dsl_replacer.add_dsl_and_replacer(r'\$\{(.*?)\}', prepare_actions_input_and_secrets)
+    prepare_actions_input_and_secrets()
     option = create_option('../assets/option/option_workflow_export_favorites.yml')
     option.call_all_plugin('main')
 
