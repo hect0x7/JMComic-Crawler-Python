684754af577e0646d73ab3cd1b80fcf03823ef83
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2024-01-17 00:57:05 +0800

    v2.5.2: 兼容v2.5.1之前的异常hook代码（v2.5.1会使得旧的GitHub Actions下载脚本报错）

--- src/jmcomic/jm_exception.py
+++ src/jmcomic/jm_exception.py
@@ -65,6 +65,13 @@ class ExceptionTool:
     CONTEXT_KEY_RE_PATTERN = 'pattern'
     CONTEXT_KEY_MISSING_JM_ID = 'missing_jm_id'
 
+    # 兼容旧版本
+
+    EXTRA_KEY_RESP = 'resp'
+    EXTRA_KEY_HTML = 'html'
+    EXTRA_KEY_RE_PATTERN = 'pattern'
+    EXTRA_KEY_MISSING_JM_ID = 'missing_jm_id'
+
     @classmethod
     def raises(cls,
                msg: str,
@@ -156,3 +163,14 @@ class ExceptionTool:
             return
 
         cls.raises(msg)
+
+    @classmethod
+    def replace_old_exception_executor(cls, raises: Callable[[Callable, str, dict], None]):
+        old = cls.raises
+
+        def new(msg, context=None, _etype=None):
+            if context is None:
+                context = {}
+            raises(old, msg, context)
+
+        cls.raises = new
