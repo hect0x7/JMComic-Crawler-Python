d71109ab3ea4560b9de95820b05ac1df5b2ee6f6
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2025-01-26 01:07:33 +0800

    v2.5.30: 更新网页端提取章节的正则表达式

--- src/jmcomic/__init__.py
+++ src/jmcomic/__init__.py
@@ -2,7 +2,7 @@
 # 被依赖方 <--- 使用方
 # config <--- entity <--- toolkit <--- client <--- option <--- downloader
 
-__version__ = '2.5.29'
+__version__ = '2.5.30'
 
 from .api import *
 from .jm_plugin import *
