8274797c94f2fb95421349e97d285030bb286168
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2025-03-08 23:23:28 +0800

    v2.5.31: pip包支持python3.13

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
