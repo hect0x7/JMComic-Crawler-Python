0f8875acc3ea24feb82281c2f401ea0446ba525d
lgzyulin <lgzyulin@qq.com>
2025-04-01 21:02:24 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -2,11 +2,14 @@ from jmcomic import *
 from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
-jm_albums = '''
+jm_albums = str_to_list('''
+456911
+178543
+16354
+451300
 
 
-
-'''
+''')
 
 # 单独下载章节
 jm_photos = '''
