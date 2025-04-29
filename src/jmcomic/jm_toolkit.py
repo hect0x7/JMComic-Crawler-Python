bfa47595459788c407128bdae26f102e9d619ef2
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2025-04-29 16:10:30 +0800

    v2.5.38: 更新网页正则表达式 (#434)

--- src/jmcomic/jm_toolkit.py
+++ src/jmcomic/jm_toolkit.py
@@ -24,7 +24,7 @@ class JmcomicText:
 
     pattern_html_album_album_id = compile(r'<span class="number">.*?：JM(\d+)</span>')
     pattern_html_album_scramble_id = compile(r'var scramble_id = (\d+);')
-    pattern_html_album_name = compile(r'<h1 class="book-name" id="book-name">([\s\S]*?)</h1>')
+    pattern_html_album_name = compile(r'<h2 class="book-name" id="book-name"[^>]*?>([\s\S]*?)</h2>')
     pattern_html_album_episode_list = compile(r'data-album="(\d+)"[^>]*>\s*?<li.*?>\s*?第(\d+)[话話]([\s\S]*?)<[\s\S]*?>')
     pattern_html_album_page_count = compile(r'<span class="pagecount">.*?:(\d+)</span>')
     pattern_html_album_pub_date = compile(r'>上架日期 : (.*?)</span>')
